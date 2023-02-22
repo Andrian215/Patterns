bc6186e51582c51913c027d7bb74161777c7cc69
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2023-02-22 20:06:41 +0200

    Update lab2_strategy.ts

--- lab2_strategy.ts
+++ lab2_strategy.ts
@@ -1,150 +1,71 @@
-enum NameOfProduct {
-    TV = "TV",
-    TheTimes = "TheTimes",
-    Headphones = "Headphones",
-    Camera = "Camera",
-    Laptop = "Laptop",
+class Product {
+  constructor(public name: string, public price: number) {}
 }
 
-enum PriceOfProduct {
-    First_Pr = 1200,
-    Second_Pr = 250,
-    Third_Pr= 120,
-    Fourth_Pr = 700,
-    Fifth_Pr = 950,
-}
+class Order {
+  private products: [Product, number][] = [];
 
-const date1 = new Date('December 24, 2022 00:00:00')
-const date2 = new Date('December 31, 2022 00:00:00');
+  public addProduct(product: Product, numOfproduct: number) {
+    this.products.push([product, numOfproduct]);
+  }
 
-enum AllTerminsOfCredit {
-    ThreeMonth = 3,
-    SixMonth = 6,
-    NineMonth = 9,
-    TwelveMonth = 12,
-}
+  public calculateTotalPrice(): number {
+    return this.products.reduce(
+      (total, [product, numOfproduct]) => total + product.price * numOfproduct,
+      0
+    );
+  }
 
-class PaymentMethods {
-    price: number;
-    termin: AllTerminsOfCredit;
-    
-    constructor(price, termin){
-        this.price = price;
-        this.termin = termin;
-    }
+  public Order(): void {
+    console.log("Order:");
+    this.products.forEach(([product, numOfproduct]) => {
+      console.log(`${product.name} x ${numOfproduct} - ${product.price}$`);
+    });
+    console.log(`Total: ${this.calculateTotalPrice()}$`);
+  }
 
-    paymentOnCredit() {
-        let graphic = '';
-        let percent = 15;
-        let getfullpricewithpercent = ((this.price * percent) / 100) + this.price;
-        let getpriceofmonth = getfullpricewithpercent / this.termin;
-        for (let i = 1; i < this.termin + 1; i++) {
-            graphic += '\n' + i  + '-month = ' + getpriceofmonth;
-        }
-        let payment = graphic + '\n' + percent + '%' + '\n' + this.termin + ' months';      
-        return payment;
-    }
-
-    PaymentInInstallments() {
-        let paymentininstallments = this.price / this.termin;
-        let pay = 'Payment is ' + paymentininstallments + ' per month';
-        return pay;
-    }
-
-    discountOnGoods() {
-        let now = new Date();
-        let newprice;
-        let percent1 = 15;
-        let percent2 = 20;
-        if (now == date1 && now < date2) {
-            newprice = this.price - (this.price * percent1) / 100;
-        }
-
-        return newprice;
-    }
-
+  public pay(paymentStrategy: Strategy): void {
+    paymentStrategy.pay(this.calculateTotalPrice());
+  }
 }
 
 interface Strategy {
-    paymentMethod(): number;
+  pay(amount: number): void;
 }
 
-class BuyersOrderOfOneProduct implements Strategy {
-    name: NameOfProduct;
-    price: PriceOfProduct;
-    methods_of_payment: PaymentMethods;
-
-    constructor(name, price, methods_of_payment) {
-        this.name = name;
-        this.price = price;
-        this.methods_of_payment = methods_of_payment;
-    }
-
-    getProductByName() {
-        return this.name;
-    }
-
-    paymentMethod(): number {
-        return this.price;
-    }
-
-    credit() {
-        return this.methods_of_payment.paymentOnCredit();
-    }
-
-    inParts() {
-        return this.methods_of_payment.PaymentInInstallments();
-    }
-
+class CardPayment implements Strategy {
+  public pay(amount: number): void {
+    console.log(`Payment by credit card in the sum ${amount}$`);
+  }
 }
 
-class BuyersOrderOfSeveralGoods implements Strategy {
-    name: NameOfProduct;
-    price: PriceOfProduct;
-    num_of_product_orders: number;
-    methods_of_payment: PaymentMethods;
-    
-    constructor(name, price, num_of_product_orders, methods_of_payment){
-        this.name = name;
-        this.price = price;
-        this.num_of_product_orders = num_of_product_orders;
-        this.methods_of_payment = methods_of_payment;
-    }
+class CreditPayment implements Strategy {
+  constructor(private readonly percent: number, private readonly term: number) {}
 
-    getProductByName() {
-        return this.name;
-    }
-
-    paymentMethod(): number {
-        let amount = 0;
-        for (let i = 0; i < this.num_of_product_orders; i++) {
-            amount += this.price;
-        }
-        return amount;
-    }
-
-    credit() {
-        return this.methods_of_payment.paymentOnCredit();
-    }
-
-    inParts() {
-        return this.methods_of_payment.PaymentInInstallments();
-    }
-
+  public pay(amount: number): void {
+    const monthlyPayment = (amount * (1 + this.percent)) / this.term;
+    console.log(`Payment in credit: ${this.term} months for ${monthlyPayment}$`);
+  }
 }
 
-const product = NameOfProduct.Headphones;
-const price_of_product = PriceOfProduct.Third_Pr;
-const term = AllTerminsOfCredit.SixMonth;
-let amount = Number(prompt('Enter amount of product: '));
-while(amount <= 0) {
-    amount = Number(prompt('Enter a number > 0: '));
-}
-let full_price = price_of_product * amount;
+const product1 = new Product("TV", 1200);
+const product2 = new Product("TheTimes", 250);
+const product3 = new Product("Headphones", 120);
+const product4 = new Product("Camera", 700);
+const product5 = new Product("Laptop", 950);
 
-const test= new PaymentMethods(full_price, term);
+const order = new Order();
+order.addProduct(product1, 2);
+order.addProduct(product2, 5);
+order.addProduct(product3, 3);
+order.addProduct(product4, 2);
+order.addProduct(product5, 1);
 
-const some_order = new BuyersOrderOfSeveralGoods(product, price_of_product, amount, test);
-console.log(some_order.getProductByName());
-console.log(some_order.paymentMethod());
-console.log(some_order.credit());
+order.Order();
+
+const cardPayment = new CardPayment();
+order.pay(cardPayment);
+
+const creditPayment = new CreditPayment(0.1, 12);
+order.pay(creditPayment);
+
